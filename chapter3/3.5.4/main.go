550ea623458641bd64e091a930732dfecf0b6eb2
roronya <roronya628@gmail.com>
2023-02-16 20:28:43 +0900

    add 3.5.4

--- chapter3/3.5.4/main.go
+++ chapter3/3.5.4/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"hash/crc32"
 	"io"
 	"os"
 )
@@ -13,6 +15,11 @@ func dumpChunk(chunk io.Reader) {
 	buffer := make([]byte, 4)
 	chunk.Read(buffer)
 	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
+	if bytes.Equal(buffer, []byte("teXt")) {
+		rawText := make([]byte, length)
+		chunk.Read(rawText)
+		fmt.Println(string(rawText))
+	}
 }
 
 func readChunks(file *os.File) []io.Reader {
@@ -33,13 +40,40 @@ func readChunks(file *os.File) []io.Reader {
 	return chunks
 }
 
+func textChunk(text string) io.Reader {
+	byteText := []byte(text)
+	crc := crc32.NewIEEE()
+	var buffer bytes.Buffer
+	binary.Write(&buffer, binary.BigEndian, int32(len(byteText)))
+	writer := io.MultiWriter(&buffer, crc)
+	io.WriteString(writer, "teXt")
+	writer.Write(byteText)
+	binary.Write(&buffer, binary.BigEndian, crc.Sum32())
+	return &buffer
+}
+
 func main() {
 	file, err := os.Open("PNG_transparency_demonstration_1.png")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
+
+	newFile, err := os.Create("PNG_transparency_demonstration_secret.png")
+	if err != nil {
+		panic(err)
+	}
+	defer newFile.Close()
+
 	chunks := readChunks(file)
+	io.WriteString(newFile, "\x89PNG\r\n\x1a\n")
+	io.Copy(newFile, chunks[0])
+	io.Copy(newFile, textChunk("Lambda Note++"))
+	for _, chunk := range chunks[1:] {
+		io.Copy(newFile, chunk)
+	}
+
+	chunks = readChunks(newFile)
 	for _, chunk := range chunks {
 		dumpChunk(chunk)
 	}
