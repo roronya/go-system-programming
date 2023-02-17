812d45bffff3f64605073811a1e84d92682d34c2
roronya <roronya628@gmail.com>
2023-02-17 23:50:58 +0900

    fix Q3.3

--- chapter3/Q3.3/main.go
+++ chapter3/Q3.3/main.go
@@ -7,37 +7,18 @@ import (
 	"strings"
 )
 
-type MyZipWriter struct {
-	zw *zip.Writer
-}
-
-func NewMyZipWriter(zw *zip.Writer) *MyZipWriter {
-	return &MyZipWriter{zw: zw}
-}
-
-func (mzw *MyZipWriter) Write(name string, r *strings.Reader) (io.Writer, error) {
-	file, err := mzw.zw.Create(name)
-	if err != nil {
-		return nil, err
-	}
-	_, err = io.Copy(file, r)
-	if err != nil {
-		return nil, err
-	}
-	return file, err
-}
-
 func main() {
-	file, err := os.Create("test.zip")
+	zipFile, err := os.Create("sample.zip")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-	zipWriter := zip.NewWriter(file)
+	defer zipFile.Close()
+	zipWriter := zip.NewWriter(zipFile)
 	defer zipWriter.Close()
-	myZipWriter := NewMyZipWriter(zipWriter)
-	_, err = myZipWriter.Write("newfile.txt", strings.NewReader("this is a test"))
+	file, err := zipWriter.Create("sample.txt")
 	if err != nil {
 		panic(err)
 	}
+	io.Copy(file, strings.NewReader("this is a sample text"))
+	zipWriter.Flush()
 }
